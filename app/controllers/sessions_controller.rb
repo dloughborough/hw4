c7307cabcf89551af062aaec223c972f2605b3aa
dloughborough <david.loughborough@kellogg.northwestern.edu>
2022-03-14 18:40:36 +0000

    updated layouts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,25 +3,25 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by({ email: params["email"] })
+    @user = User.where({ email: params["email"] })[0]
     if @user
       if BCrypt::Password.new(@user.password) == params["password"]
         session[:user_id] = @user.id
-        flash[:notice] = "Logged in as: #{@user.username}"
+        flash[:notice] = "Logged in as #{@user.username}"
         redirect_to "/places"
       else
-        flash[:notice] = "Nope"
+        flash[:notice] = "Nope."
         redirect_to "/sessions/new"
       end
     else
-      flash[:notice] = "Nope"
+      flash[:notice] = "Nope."
       redirect_to "/sessions/new"
     end
   end
-
+  
   def destroy
     session[:user_id] = nil
-    redirect_to "sessions/new"
+    flash[:notice] = "Goodbye."
+    redirect_to "/sessions/new"
   end
 end
-  
